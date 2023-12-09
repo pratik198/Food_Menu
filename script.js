951f025fcbfe857eb955053c05763b651fb53a23
unknown <routpratik09@gmail.com>
2023-12-09 15:05:56 +0530

    c

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const foodData = [
     {
+        image: 'https://b.zmtcdn.com/data/reviews_photos/f6f/054915ad0da692a94bd39a72db912f6f_1606498650.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*',
+        foodname: 'Chilis Grill & Bar',
+        about: 'Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Sahid Nagar restaurants, Best Sahid Nagar restaurants, Bhubaneshwar City restaurants, Casual Dining in Bhubaneswar, Casual Dining near me, Casual Dining in Sahid Nagar, Bar in Bhubaneswar, Bar near me, Bar in Sahid Nagar, in Bhubaneswar, near me, in Sahid Nagar, Order food online in Sahid Nagar, Order food online in Bhubaneswar',
+        price: '123',
+        type:"breakfast",
+      },
+    {
       image:
         "https://b.zmtcdn.com/data/pictures/5/2900405/3218168b9e8dddebe3f1dad5a6e89b20.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
       foodname: "Green Chillyz",
       about:
         "1, greenchillyz the fastfoodmastercanteen bhubaneswar odisha, greenchillyz the fast foodmastercanteen square bhubaneswar odisha, green chillyz ravi talkies, green chilli master canteen bhubaneswar menu",
       price: "250",
+      type:"breakfast",
     },
     {
       image:
@@ -14,6 +22,7 @@ const foodData = [
       about:
         "Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Jatni restaurants, Best Jatni restaurants, Bhubaneshwar City restaurants, Quick Bites in Bhubaneswar, Quick Bites near me, Quick Bites in Jatni, in Bhubaneswar, near me, in Jatni, in Bhubaneswar, near me, in Jatni",
       price: "350",
+      type:"breakfast",
     },
     {
       image:
@@ -22,6 +31,7 @@ const foodData = [
       about:
         "Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Kharabela Nagar restaurants, Best Kharabela Nagar restaurants, Bhubaneshwar City restaurants, Quick Bites in Bhubaneswar, Quick Bites near me, Quick Bites in Kharabela Nagar, in Bhubaneswar, near me, in Kharabela Nagar, in Bhubaneswar, near me, in Kharabela Nagar, Order food online in Kharabela Nagar, Order food online in Bhubaneswar",
       price: "150",
+      type:"lunch",
     },
     {
       image:
@@ -30,6 +40,7 @@ const foodData = [
       about:
         "Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Jayadev Vihar restaurants, Best Jayadev Vihar restaurants, Bhubaneshwar City restaurants, Casual Dining in Bhubaneswar, Casual Dining near me, Casual Dining in Jayadev Vihar, in Bhubaneswar, near me, in Jayadev Vihar, Order food online in Jayadev Vihar, Order food online in Bhubaneswar",
       price: "450",
+      type:"lunch",
     },
     {
       image:
@@ -38,6 +49,7 @@ const foodData = [
       about:
         "Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Unit 4 restaurants, Best Unit 4 restaurants, Bhubaneshwar City restaurants, Casual Dining in Bhubaneswar, Casual Dining near me, Casual Dining in Unit 4, in Bhubaneswar, near me, in Unit 4, in Bhubaneswar, near me, in Unit 4, Order food online in Unit 4, Order food online in Bhubaneswar",
       price: "200",
+      type:"shakes",
     },
     {
       image:
@@ -46,6 +58,7 @@ const foodData = [
       about:
         "Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Kharabela Nagar restaurants, Best Kharabela Nagar restaurants, Bhubaneshwar City restaurants, Order food online in Kharabela Nagar, Order food online in Bhubaneswar",
       price: "520",
+      type:"shakes",
     },
     {
       image:
@@ -54,31 +67,48 @@ const foodData = [
       about:
         "Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Jayadev Vihar restaurants, Best Jayadev Vihar restaurants, Bhubaneshwar City restaurants, Sweet Shop in Bhubaneswar, Sweet Shop near me, Sweet Shop in Jayadev Vihar, Quick Bites in Bhubaneswar, Quick Bites near me, Quick Bites in Jayadev Vihar, Order food online in Jayadev Vihar, Order food online in Bhubaneswar",
       price: "1130",
+      type:"dinner",
     },
     {
       image: 'https://b.zmtcdn.com/data/reviews_photos/549/e1165b07ec036e70c639027be1433549_1518435017.jpeg?fit=around|771.75:416.25&crop=771.75:416.25;*,*',
       foodname: 'Cream Stone Ice Cream',
       about: 'Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Jayadev Vihar restaurants, Best Jayadev Vihar restaurants, Bhubaneshwar City restaurants, Dessert Parlour in Bhubaneswar, Dessert Parlour near me, Dessert Parlour in Jayadev Vihar, in Bhubaneswar, near me, in Jayadev Vihar, Order food online in Jayadev Vihar, Order food online in Bhubaneswar',
-      price: '120'
+      price: '120',
+      type:"dinner",
     },
     {
       image: 'https://b.zmtcdn.com/data/pictures/chains/0/2900050/a2ada83dadf8a33298456a5f6000ee97_o2_featured_v2.jpg',
       foodname: 'Shree Kunj Restaurant',
       about: 'Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Ashok Nagar restaurants, Best Ashok Nagar restaurants, Bhubaneshwar City restaurants, Casual Dining in Bhubaneswar, Casual Dining near me, Casual Dining in Ashok Nagar, in Bhubaneswar, near me, in Ashok Nagar, Order food online in Ashok Nagar, Order food online in Bhubaneswar',
-      price: '420'
+      price: '420',
+      type:"dinner",
     },
     {
       image: 'https://b.zmtcdn.com/data/reviews_photos/f6f/054915ad0da692a94bd39a72db912f6f_1606498650.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*',
       foodname: 'Chilis Grill & Bar',
       about: 'Restaurants in Bhubaneshwar, Bhubaneshwar Restaurants, Sahid Nagar restaurants, Best Sahid Nagar restaurants, Bhubaneshwar City restaurants, Casual Dining in Bhubaneswar, Casual Dining near me, Casual Dining in Sahid Nagar, Bar in Bhubaneswar, Bar near me, Bar in Sahid Nagar, in Bhubaneswar, near me, in Sahid Nagar, Order food online in Sahid Nagar, Order food online in Bhubaneswar',
-      price: '123'
+      price: '123',
+      type:"breakfast",
     }
   ];
   
-  const foodMenu = document.querySelector('.food-menu')
-  const foodItems = document.querySelector('.food-items')
+const card = document.getElementById("item-2");
+const mainMenu = document.querySelector('.food-menu');
+
+function clone(food) {
+    console.log(food)
+  const cloneDiv = card.cloneNode(true);
+  cloneDiv.id ='';
+  cloneDiv.querySelector(".f_Name_1").innerText = food.foodname;
+  cloneDiv.querySelector(".f_price_1").innerText = `₹${food.price}`;
+  cloneDiv.querySelector(".f_Image_1 img").src = food.image;
+  cloneDiv.querySelector('.f_about-1').innerText = food.about;
+
+  return cloneDiv;
+}
+
+foodData.forEach(food => {
+  const cloneData = clone(food);
+  mainMenu.appendChild(cloneData);
   
-  foodData.forEach((items) => {
-  
-  
-  })
+});
